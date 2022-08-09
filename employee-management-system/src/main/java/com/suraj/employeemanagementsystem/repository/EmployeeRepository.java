3e47c9c111c0a2da3858de16c8a09b0324bda531
zarus101 <83441959+zarus101@users.noreply.github.com>
2022-08-09 22:34:32 +0545

    ss

---
+++ employee-management-system/src/main/java/com/suraj/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.suraj.employeemanagementsystem.repository;
+
+import com.suraj.employeemanagementsystem.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
---
+++ employee-management-system/src/main/java/com/suraj/employeemanagementsystem/services/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.suraj.employeemanagementsystem.services;
+
+import com.suraj.employeemanagementsystem.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
